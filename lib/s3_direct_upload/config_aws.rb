1df9b6388bca69cf77937e310b2dd784104ea693
ikusuki <koki142@gmail.com>
2013-06-26 16:32:01 +0900

    Moving AWS check to form helper

--- lib/s3_direct_upload/config_aws.rb
+++ lib/s3_direct_upload/config_aws.rb
@@ -7,22 +7,6 @@ module S3DirectUpload
     ATTRIBUTES = [:access_key_id, :secret_access_key, :bucket, :prefix_to_clean, :region, :url]
 
     attr_accessor *ATTRIBUTES
-
-    def self.aws_access_key_id
-      if self.access_key_id.blank?
-        return AWS.config().credentials()[:access_key_id] if Object.const_defined?('AWS')
-      else
-        self.access_key_id
-      end
-    end
-
-    def self.aws_secret_access_key
-      if self.secret_access_key.blank?
-        return AWS.config().credentials()[:secret_access_key] if Object.const_defined?('AWS')
-      else
-        self.secret_access_key
-      end
-    end
   end
 
   def self.config
