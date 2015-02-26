a284a0fe1453a206c0f64281e065be13fa81d084
Jan Raasch <jan@janraasch.com>
2015-02-26 16:35:38 +0100

    Fix upstream specs
    Those guys were failing were a looooooooming looooooooong time :)

--- spec/existance_spec.rb
+++ spec/existance_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 describe S3DirectUpload do
   it "version must be defined" do
-    S3DirectUpload::VERSION.should be_true
+    S3DirectUpload::VERSION.should be_truthy
   end
 
   it "config must be defined" do
-    S3DirectUpload.config.should be_true
+    S3DirectUpload.config.should be_truthy
   end
 
 end
