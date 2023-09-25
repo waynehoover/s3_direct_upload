43e86890f7aec171287a259ee999cf3ad5df9e11
re-pete-r <52224390+spam-elgoog@users.noreply.github.com>
2023-09-25 11:26:15 -0400

    nada

--- lib/s3_direct_upload/form_helper.rb
+++ lib/s3_direct_upload/form_helper.rb
@@ -2,11 +2,17 @@ module S3DirectUpload
   module UploadHelper
     def s3_uploader_form(options = {}, &block)
       uploader = S3Uploader.new(options)
-      form_tag(uploader.url, uploader.form_options) do
+      content_tag(:div, uploader.wrapper_options) do
         uploader.fields.map do |name, value|
           hidden_field_tag(name, value)
         end.join.html_safe + capture(&block)
       end
+    end
+
+    alias_method :s3_uploader, :s3_uploader_form
+
+    def s3_uploader_url ssl = true
+      S3DirectUpload.config.url || "http#{ssl ? 's' : ''}://#{S3DirectUpload.config.region || "s3"}.amazonaws.com/#{S3DirectUpload.config.bucket}/"
     end
 
     class S3Uploader
@@ -16,8 +22,6 @@ module S3DirectUpload
           aws_access_key_id: S3DirectUpload.config.access_key_id,
           aws_secret_access_key: S3DirectUpload.config.secret_access_key,
           bucket: options[:bucket] || S3DirectUpload.config.bucket,
-          region: S3DirectUpload.config.region || "us-east-1",
-          url: S3DirectUpload.config.url,
           ssl: true,
           acl: "public-read",
           expiration: 10.hours.from_now.utc.iso8601,
@@ -26,26 +30,14 @@ module S3DirectUpload
           callback_param: "file",
           key_starts_with: @key_starts_with,
           key: key,
-          date: Time.now.utc.strftime("%Y%m%d"),
-          timestamp: Time.now.utc.strftime("%Y%m%dT%H%M%SZ")
+          server_side_encryption: nil
         )
       end
 
-      def hostname
-        if @options[:region] == "us-east-1"
-          "#{@options[:bucket]}.s3.amazonaws.com"
-        else
-          "#{@options[:bucket]}.s3-#{@options[:region]}.amazonaws.com"
-        end
-      end
-
-      def form_options
+      def wrapper_options
         {
           id: @options[:id],
           class: @options[:class],
-          method: "post",
-          authenticity_token: false,
-          multipart: true,
           data: {
             callback_url: @options[:callback_url],
             callback_method: @options[:callback_method],
@@ -56,24 +48,19 @@ module S3DirectUpload
 
       def fields
         {
-          :acl => @options[:acl],
           :key => @options[:key] || key,
+          :acl => @options[:acl],
+          "AWSAccessKeyId" => @options[:aws_access_key_id],
           :policy => policy,
+          :signature => signature,
           :success_action_status => "201",
-          'X-Amz-Algorithm' => 'AWS4-HMAC-SHA256',
-          'X-Amz-Credential' => "#{@options[:aws_access_key_id]}/#{@options[:date]}/#{@options[:region]}/s3/aws4_request",
-          'X-Amz-Date' => @options[:timestamp],
-          'X-Amz-Signature' => signature,
-          'X-Requested-With' => 'xhr'
-        }
+          'X-Requested-With' => 'xhr',
+          "x-amz-server-side-encryption" => @options[:server_side_encryption]
+        }.delete_if { |k, v| v.nil? }
       end
 
       def key
         @key ||= "#{@key_starts_with}{timestamp}-{unique_id}-#{SecureRandom.hex}/${filename}"
-      end
-
-      def url
-        @options[:url] || "http#{@options[:ssl] ? 's' : ''}://#{hostname}/"
       end
 
       def policy
@@ -84,33 +71,32 @@ module S3DirectUpload
         {
           expiration: @options[:expiration],
           conditions: [
-            ["starts-with", "$utf8", ""],
             ["starts-with", "$key", @options[:key_starts_with]],
             ["starts-with", "$x-requested-with", ""],
             ["content-length-range", 0, @options[:max_file_size]],
             ["starts-with","$content-type", @options[:content_type_starts_with] ||""],
             {bucket: @options[:bucket]},
             {acl: @options[:acl]},
-            {success_action_status: "201"},
-            {'X-Amz-Algorithm' => 'AWS4-HMAC-SHA256'},
-            {'X-Amz-Credential' => "#{@options[:aws_access_key_id]}/#{@options[:date]}/#{@options[:region]}/s3/aws4_request"},
-            {'X-Amz-Date' => @options[:timestamp]}
-          ] + (@options[:conditions] || [])
+            {success_action_status: "201"}
+          ] + server_side_encryption + (@options[:conditions] || [])
         }
       end
 
-      def signing_key
-        #AWS Signature Version 4
-        kDate    = OpenSSL::HMAC.digest('sha256', "AWS4" + @options[:aws_secret_access_key], @options[:date])
-        kRegion  = OpenSSL::HMAC.digest('sha256', kDate, @options[:region])
-        kService = OpenSSL::HMAC.digest('sha256', kRegion, 's3')
-        kSigning = OpenSSL::HMAC.digest('sha256', kService, "aws4_request")
-
-        kSigning
+      def server_side_encryption
+        if @options[:server_side_encryption]
+          [ { "x-amz-server-side-encryption" => @options[:server_side_encryption] } ]
+        else
+          []
+        end
       end
 
       def signature
-        OpenSSL::HMAC.hexdigest('sha256', signing_key, policy)
+        Base64.encode64(
+          OpenSSL::HMAC.digest(
+            OpenSSL::Digest.new('sha1'),
+            @options[:aws_secret_access_key], policy
+          )
+        ).gsub("\n", "")
       end
     end
   end
